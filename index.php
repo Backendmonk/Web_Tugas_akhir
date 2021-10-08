3c20e8cae9d2e21c85f0c13bc578d75ec935b59f
Backendmonk <aryapratamaputra888@gmail.com>
2021-10-08 16:18:26 +0800

    update login
    ngubah remember me jadi register

--- index.php
+++ index.php
@@ -48,10 +48,7 @@
               </div>
               
               <div class="d-flex mb-5 align-items-center">
-                <label class="control control--checkbox mb-0"><span class="caption">Remember me</span>
-                  <input type="checkbox" checked="checked"/>
-                  <div class="control__indicator"></div>
-                </label>
+              <label for="password" ><a style= " text-decoration: none;Color : white;"href="" >Register</a></label>
               </div>
 
               <input type="submit" value="Log In" class="btn btn-block btn-primary">
