d5946f2ea2b9ff8364ea85fdd5da02ed2eab9bf9
Backendmonk <aryapratamaputra888@gmail.com>
2021-11-02 23:39:01 +0800

    update tittle

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 <meta charset="utf-8">
 <link rel="stylesheet" href="LogCSS/style.css">
-<title>Css Style Service Box</title>
+<title>Login Area</title>
 </head>
 
 <body>
