589673af06bb5d9c2f70f292085d2bc81bdd286a
OniChill <58246777+MellanaAriArt@users.noreply.github.com>
2021-11-11 03:17:51 +0800

    frond end struktur organisasi dan kegiatan ormawa

---
+++ Kemahasiswaan/Kegiatan-ORMAWA.php
@@ -0,0 +1,133 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<?php
+
+        // include "SessionKemahasiswaan.php";
+
+?>
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SB Admin 2 - Dashboard</title>
+
+<?php include '../template/head.php' ?>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+<meta charset="utf-8">
+<link rel="stylesheet" href="../LogCSS/style.css">
+
+
+
+</head>
+
+<body id="page-top">
+<?php include 'template/navbar.php' ?>
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+    <?php include 'template/sidebar.php' ?>
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column pt-4">
+
+            <!-- Main Content -->
+            <div id="content">
+                
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+
+            <div class="row mb-2">
+            <div class=" col-sm-6  text-center" >
+                <a style ="text-decoration:none;" href="http://localhost/web_tugas_akhir/Ormawa/login/login.php">
+                    <div class="serviceBox  border" style="height: 200px; padding:50px" >
+                            <i style="font-size: 40px; padding-left:-100px" class="fa fa-rocket" style="width: 80px;"></i>
+                            <h3 class="title">Pengajuan Kegiatan</h3>
+                    </div>
+                    </a>
+                </div>
+
+                <div class=" col-sm-6  text-center" >
+                <a style ="text-decoration:none;" href="http://localhost/web_tugas_akhir/Ormawa/login/login.php">
+                    <div class="serviceBox border" style="height: 200px; padding:50px" >
+                            <i style="font-size: 40px; padding-left:-100px" class="fa fa-rocket" style="width: 80px;"></i>
+                            <h3 class="title">Kegiatan ORMAWA</h3>
+                    </div>
+                    </a>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+  
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
+
+
+
+</div>
+<!-- End of Content Wrapper -->
+
+</div>
+<!-- End of Page Wrapper -->
+<!-- Footer -->
+<?php include '../template/footer.php' ?>
+<!-- End of Footer -->
+<!-- Scroll to Top Button-->
+<a class="scroll-to-top rounded" href="#page-top">
+<i class="fas fa-angle-up"></i>
+</a>
+
+<!-- Logout Modal-->
+<div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+aria-hidden="true">
+<div class="modal-dialog" role="document">
+<div class="modal-content">
+<div class="modal-header">
+<h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+<button class="close" type="button" data-dismiss="modal" aria-label="Close">
+    <span aria-hidden="true">×</span>
+</button>
+</div>
+<div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+<div class="modal-footer">
+<button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+<a class="btn btn-primary" href="login/logout.php">Logout</a>
+</div>
+</div>
+</div>
+</div>
+
+<!-- Bootstrap core JavaScript-->
+<script src="../vendor/jquery/jquery.min.js"></script>
+<script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="../js/sb-admin-2.min.js"></script>
+
+<!-- Page level plugins -->
+<script src="../vendor/chart.js/Chart.min.js"></script>
+
+<!-- Page level custom scripts -->
+<script src="../js/demo/chart-area-demo.js"></script>
+<script src="../js/demo/chart-pie-demo.js"></script>
+
+</body>
+
+</html>
+
+</body>
+</html>
---
+++ Kemahasiswaan/Struktur-WKIII.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+
+        include "SessionPembina.php";
+
+?>
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SB Admin 2 - Dashboard</title>
+
+<?php include '../template/head.php' ?>
+
+</head>
+
+<body id="page-top">
+<?php include 'template/navbar.php' ?>
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+
+    <?php include 'template/sidebar.php' ?>
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column pt-4">
+
+            <!-- Main Content -->
+            <div id="content">
+                
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <div class=" text-center  mb-4">
+                        <h3 <span class="text-danger">STRUKTUR ORGANISASI</span> <span class=" text-gray-500"> WAKIL KETUA III</span> </h3>
+                    </div>
+
+                    <!-- Content Row -->
+                    <div class="row">
+                        <main class="col overflow-auto h-100">
+                            <div class="row text-center mb-5">
+                                <div class="col">
+                                    <img class="border border-3 rounded-circle" src="../img/LogoStiki.png" alt="" width="150px">
+                                </div>
+                            </div>
+                            <div class="row text-center mb-5">
+                                <div class="col">
+                                    <img class="border border-3 rounded-circle" src="../img/LogoStiki.png" alt=""  width="150px">
+                                </div>
+                                <div class="col">
+                                    <img class="border border-3 rounded-circle" src="../img/LogoStiki.png" alt=""  width="150px">
+                                </div>
+                                <div class="col">
+                                    <img class="border border-3 rounded-circle" src="../img/LogoStiki.png" alt=""  width="150px">
+                                </div>
+                            </div>
+                            <div class="row text-center mb-5 px-5">
+                            <div class="col">
+                                    <img class="border border-3 rounded-circle" src="../img/LogoStiki.png" alt=""  width="150px">
+                                </div>
+                                <div class="col">
+                                    <img class="border border-3 rounded-circle" src="../img/LogoStiki.png" alt=""  width="150px">
+                                </div>
+                            </div>
+                            </div>
+             
+                        </main>
+                    </div>
+
+                    <!-- Content Row -->
+
+                    <div class="row">
+
+                      
+                    </div>
+
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+        
+
+        </div>
+        <!-- End of Content Wrapper -->
+    
+    </div>
+    <!-- End of Page Wrapper -->
+<!-- Footer -->
+<?php include '../template/footer.php' ?>
+            <!-- End of Footer -->
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+    <!-- Logout Modal-->
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                    <a class="btn btn-primary" href="login/logout.php">Logout</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="../vendor/jquery/jquery.min.js"></script>
+    <script src="../vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="../vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="../js/sb-admin-2.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="../vendor/chart.js/Chart.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="../js/demo/chart-area-demo.js"></script>
+    <script src="../js/demo/chart-pie-demo.js"></script>
+
+</body>
+
+</html>
