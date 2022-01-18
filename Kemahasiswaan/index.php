28ad04d4d1df7c8d918b6d98b7a3a016038a9573
Backendmonk <aryapratamaputra888@gmail.com>
2022-01-18 16:52:35 +0800

    update index

--- Kemahasiswaan/index.php
+++ Kemahasiswaan/index.php
@@ -57,19 +57,7 @@
                     <main class="col overflow-auto h-100">
             <div class="bg-light border rounded-3 p-3">
               
-                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Next</span>
-                        </a>
-
-                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-                             <script src="https://code.highcharts.com/highcharts.js"></script>
+            <script src="https://code.highcharts.com/highcharts.js"></script>
                             <script src="https://code.highcharts.com/modules/exporting.js"></script>
                             <script src="https://code.highcharts.com/modules/export-data.js"></script>
                             <script src="https://code.highcharts.com/modules/accessibility.js"></script>
@@ -79,15 +67,6 @@
                                 
                             </figure>
 
-                            </div>
-                            
-                            <div class="carousel-item">
-                            <?php
-                                include "../WKIII/Struktur-WKIII.php";
-                            ?>
-                            </div>
-                            
-                        
                         </div>
                        
                         </div>
